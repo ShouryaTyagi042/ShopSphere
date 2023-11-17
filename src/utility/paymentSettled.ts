2ab9469c3fb1810473a148542e37f10f16a3c525
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-11-18 00:30:29 +0530

    modified cron job

--- src/utility/paymentSettled.ts
+++ src/utility/paymentSettled.ts
@@ -1,4 +1,5 @@
 import Order from "../models/order";
+import Product from "../models/product";
 import Seller from "../models/seller";
 import "dotenv/config";
 
@@ -10,8 +11,11 @@ export const paymentSettled = cron.schedule('*/5 * * * *', async () => {
         cursor.forEach(async (order) => {
             const products = order.products;
             products.forEach(async (product: any) => {
-                const seller = await Seller.findOne({ email: product.seller })
+                const productInfo = await Product.findById(product.productId)
+                const seller = await Seller.findOne({ email: productInfo?.seller })
+                console.log(seller);
                 seller!.income += (product.price * product.quantity) * 0.8;
+                console.log(seller?.income);
                 await seller!.save();
             })
             order.paymentSettled = true;
